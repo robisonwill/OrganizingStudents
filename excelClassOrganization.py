98fd7e908fd8e7618207f0631a1d59d9e001c13b
robisonwill <will.robison2203@gmail.com>
2025-04-01 14:50:58 -0600

    Update excelClassOrganization.py

--- excelClassOrganization.py
+++ excelClassOrganization.py
@@ -1,11 +1,18 @@
+import openpyxl
 from openpyxl import Workbook
-from openpyxl import Poorly_Organized_Data_1
 
+importedWorkbook = openpyxl.load_workbook("Poorly_Organized_Data_1.xlsx")
 
-'''
-for row in currentWs.iter_rows(min_row= 1, max_row= 10, min_col= 1, max_col= 1) :
+currSheet = importedWorkbook.active
+
+createWorkbook = Workbook()
+
+newSheets = []
+for row in currSheet.iter_rows(min_row= 1, max_row= 10, min_col= 1, max_col= 1) :
     for cell in row :
         if cell.value not in newSheets :
-            myWorkbook.create_sheet(f"{cell.value}")
+            createWorkbook.create_sheet(f"{cell.value}")
             newSheets.append(cell.value)
-'''
+        else:
+            pass
+
